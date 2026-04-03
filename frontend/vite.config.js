98add947b980767322b2c51e186dae8a89acd5ab
Chirag Dulera <140916086+Chirag1522@users.noreply.github.com>
2026-04-03 15:37:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
     proxy: {
@@ -12,4 +13,10 @@ export default defineConfig({
       },
     },
   },
+
+  // 🔥 THIS FIXES YOUR RENDER ERROR
+  preview: {
+    host: true,
+    allowedHosts: "all"
+  }
 })
